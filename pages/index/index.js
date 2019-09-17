6f979d2dbc34bd329997c847494dc8fed42b0689
xl123456 <xileilei@yodoo.net.cn>
2019-09-17 11:46:14 +0800

    热映列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,9 @@ const app = getApp()
 
 Page({
   data: {
+
+    currentTab: 0,
+    tabStr: ['正在热映', '即将上映'],
     douBanApikey: '0df993c66c0c636e29ecbb5344252a4a',
     imgUrls: [
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568039048924&di=950b691ea199cf32a90bde81f915140f&imgtype=0&src=http%3A%2F%2Fn.sinaimg.cn%2Fent%2Ftransform%2F184%2Fw630h354%2F20190212%2F_4Op-hswimzy4836776.jpg',
@@ -12,15 +15,23 @@ Page({
       'http://img1.cache.netease.com/catchpic/1/18/18D3B0BCE16EF386F6D7B7E37CBD254D.jpg',
       'http://img4.imgtn.bdimg.com/it/u=1475771496,1520974708&fm=26&gp=0.jpg'
     ],
+    imgUrlsFuture: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568699801376&di=5af9e7b938ce3db7b7786dc7b6c7f36e&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20190903%2Fe19b0f8b291c4bc2a3d962537b92a5ba.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568700029762&di=3691089413fae98149c503541d860a98&imgtype=0&src=http%3A%2F%2Fimg.rednet.cn%2F2019%2F09-09%2F7cdbee07-1449-4255-8eb1-8a468faae65b.jpg'
+    ],
     indicatorDots: true,
     autoplay: true,
     interval: 2000,
     duration: 500,
-    movies: null
+    movies: null,
+    moviesFuture: null,
+    showLeft: true,
+    showRight: false,
   },
 
-  onLoad: function () {
+  onLoad: function() {
     this.loadMovie();
+    this.loadMovieFuture();
   },
 
   /**
@@ -50,6 +61,32 @@ Page({
   },
 
   /**
+   * 即将上映
+   */
+  loadMovieFuture() {
+    // wx.showToast({
+    //   title: '正在加载',
+    //   icon: 'loading',
+    //   duration: 10000
+    // })
+    let that = this;
+    wx.request({
+      url: 'http://api.douban.com/v2/movie/coming?apikey=' + this.data.douBanApikey,
+      method: 'GET',
+      header: {
+        'content-type': 'json'
+      },
+      success(res) {
+        console.log(res)
+        that.setData({
+          moviesFuture: res.data.entries
+        })
+        // wx.hideToast()
+      }
+    })
+  },
+
+  /**
    * 电影详情
    */
   itemClick(res) {
@@ -58,5 +95,32 @@ Page({
     wx.navigateTo({
       url: '/pages/movieDetail/movieDetail?movieId=' + this.data.movies[index].id,
     })
-  }
+  },
+
+  tabTap(e) {
+    console.log(e);
+    var idx = e.currentTarget.dataset.idx;
+    this.setData({
+      currentTab: idx,
+    });
+
+    if (this.data.currentTab == 0) {
+      console.log('1111111111=');
+      this.setData({
+        showLeft: true,
+        showRight: false
+      })
+
+    } else if (this.data.currentTab == 1) {
+      console.log('222222222222=');
+      this.setData({
+        showLeft: false,
+        showRight: true
+      })
+    };
+    console.log('showRight=' + this.data.showRight);
+  },
+
+
+
 })
